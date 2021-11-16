6b257c619ced3b134d90ff35bf14467ce9f75e7f
JJefro <jevjefro@gmail.com>
2021-11-16 13:33:13 +0200

    code refactoring

---
+++ WeatherForecastBrickTests/WeatherForecastBrickSnapshotTests.swift
@@ -0,0 +1,78 @@
+//
+//  WeatherForecastBrickTests.swift
+//  WeatherForecastBrickTests
+//
+//  Created by Jevgenijs Jefrosinins on 07/11/2021.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import WeatherForecastBrick
+
+class WeatherForecastBrickSnapshotTests: XCTestCase {
+
+    func test_weatherRain() throws {
+        let sut = sutBuild(weather: .mockRain)
+        assertSnapshot(matching: sut, as: .image)
+    }
+
+    func test_weatherSnow() throws {
+        let sut = sutBuild(weather: .mockSnow)
+        assertSnapshot(matching: sut, as: .image)
+    }
+
+    func test_weatherWindy() throws {
+        let sut = sutBuild(weather: .mockWind)
+        assertSnapshot(matching: sut, as: .image)
+    }
+
+    func test_thunderstorm() throws {
+        let sut = sutBuild(weather: .mockThunderstorm)
+        assertSnapshot(matching: sut, as: .image)
+    }
+
+    func test_weatherFog() throws {
+        let sut = sutBuild(weather: .mockFog)
+        assertSnapshot(matching: sut, as: .image)
+    }
+
+    func test_weatherSunny() throws {
+        let sut = sutBuild(weather: .mockSunny)
+        assertSnapshot(matching: sut, as: .image)
+    }
+
+    private func sutBuild(weather: WeatherEntity) -> UIViewController {
+        let mockModel = MockWeatherModel(mockWeather: weather)
+        let sut = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateInitialViewController() as! MainViewController
+        sut.model = mockModel
+        return sut
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
+
+class MockWeatherModel: WeatherModelProtocol {
+
+    let mockWeather: WeatherEntity
+    weak var delegate: WeatherModelDelegate?
+
+    init(mockWeather: WeatherEntity) {
+        self.mockWeather = mockWeather
+    }
+
+    func updateWeatherAtCurrentLocation() {
+        delegate?.weatherModel(self, didUpdate: mockWeather)
+    }
+    
+    func updateWeatherAt(city: String) {
+    }
+
+    func updateWeatherAtCity() {
+    }
+
+}
--- WeatherForecastBrickTests/WeatherForecastBrickTests.swift
+++ WeatherForecastBrickTests/WeatherForecastBrickTests.swift
@@ -2,77 +2,31 @@
 //  WeatherForecastBrickTests.swift
 //  WeatherForecastBrickTests
 //
-//  Created by Jevgenijs Jefrosinins on 07/11/2021.
+//  Created by Jevgenijs Jefrosinins on 16/11/2021.
 //
 
 import XCTest
-import SnapshotTesting
-@testable import WeatherForecastBrick
 
 class WeatherForecastBrickTests: XCTestCase {
 
-    func test_weatherRain() throws {
-        let sut = sutBuild(weather: .mockRain)
-        assertSnapshot(matching: sut, as: .image)
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    func test_weatherSnow() throws {
-        let sut = sutBuild(weather: .mockSnow)
-        assertSnapshot(matching: sut, as: .image)
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func test_weatherWindy() throws {
-        let sut = sutBuild(weather: .mockWind)
-        assertSnapshot(matching: sut, as: .image)
-    }
-
-    func test_thunderstorm() throws {
-        let sut = sutBuild(weather: .mockThunderstorm)
-        assertSnapshot(matching: sut, as: .image)
-    }
-
-    func test_fog() throws {
-        let sut = sutBuild(weather: .mockFog)
-        assertSnapshot(matching: sut, as: .image)
-    }
-
-    func test_sunny() throws {
-        let sut = sutBuild(weather: .mockSunny)
-        assertSnapshot(matching: sut, as: .image)
-    }
-
-    private func sutBuild(weather: WeatherEntity) -> UIViewController {
-        let mockModel = MockWeatherModel(mockWeather: weather)
-        let sut = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateInitialViewController() as! MainViewController
-        sut.model = mockModel
-        return sut
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
-    }
-}
-
-class MockWeatherModel: WeatherModelProtocol {
-
-    let mockWeather: WeatherEntity
-    weak var delegate: WeatherModelDelegate?
-
-    init(mockWeather: WeatherEntity) {
-        self.mockWeather = mockWeather
-    }
-
-    func updateWeatherAtCurrentLocation() {
-        delegate?.weatherModel(self, didUpdate: mockWeather)
-    }
-    
-    func updateWeatherAt(city: String) {
-    }
-
-    func updateWeatherAtCity() {
     }
 
 }
