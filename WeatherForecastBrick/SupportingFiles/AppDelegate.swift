7b1309b419ba446c7b11a0925b4fd6286c67cf95
JJefro <jevjefro@gmail.com>
2021-11-26 13:46:39 +0200

    code refactoring

--- WeatherForecastBrick/SupportingFiles/AppDelegate.swift
+++ WeatherForecastBrick/SupportingFiles/AppDelegate.swift
@@ -45,7 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "ShoppingList")
+        let container = NSPersistentContainer(name: "Cities")
         container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
